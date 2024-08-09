4bfd51dc660a21c03d0907189980e83a2da16bbf
Joe Thorley <joe@poissonconsulting.ca>
2024-08-09 08:10:06 -0700

    update author

--- scripts/build.R
+++ scripts/build.R
@@ -1,11 +1,20 @@
 roxygen2md::roxygen2md()
-styler::style_pkg(filetype = c("R", "Rmd"))
+
+styler::style_pkg(
+  scope = "line_breaks",
+  filetype = c("R", "Rmd")
+)
+
+lintr::lint_package(linters = linters_with_defaults(
+  line_length_linter = line_length_linter(1000),
+  object_name_linter = object_name_linter(regexes = ".*"))
+)
+
 lintr::lint_package()
 
 devtools::test()
 devtools::document()
 
-rmarkdown::render("README.Rmd", output_format = "md_document")
-pkgdown::build_site()
+pkgdown::build_home()
 
 devtools::check()
